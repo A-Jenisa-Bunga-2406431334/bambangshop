fb4f9eb68e91117718184279e738eacbdd29dfe5
jebs-tech <bungasaragih688@gmail.com>
2026-03-31 18:39:05 +0700

    Fix subscribe and unsubscribe functions to use uppercase product type.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,3 +1,4 @@
+use rocket::response::status::Created;
 use rocket::serde::json::Json;
 use bambangshop::Result;
 use crate::model::subscriber::Subscriber;
@@ -6,16 +7,16 @@ use crate::service::notification::NotificationService;
 use crate::service::product::ProductService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
-pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Json<Subscriber>> {
+pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
     return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
-        Ok(f) => Ok(Json::from(f)),
+        Ok(f) => Ok(Created::new("/").body(Json::from(f))),
         Err(e) => Err(e)
     };
 }
 
-#[post("/unsubscribe/<product_type>/<subscriber_url>")]
-pub fn unsubscribe(product_type: &str, subscriber_url: &str) -> Result<Json<Subscriber>> {
-    return match NotificationService::unsubscribe(product_type, subscriber_url) {
+#[post("/unsubscribe/<product_type>?<url>")]
+pub fn unsubscribe(product_type: &str, url: &str) -> Result<Json<Subscriber>> {
+    return match NotificationService::unsubscribe(product_type, url) {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     };
--- src/service/notification.rs
+++ src/service/notification.rs
@@ -1,5 +1,5 @@
-use rocket::http::Status;
 use bambangshop::{Result, compose_error_response};
+use rocket::http::Status;
 use crate::model::notification::Notification;
 use crate::model::product::Product;
 use crate::model::subscriber::Subscriber;
@@ -9,19 +9,23 @@ pub struct NotificationService;
 
 impl NotificationService {
     pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
-        let subscriber_result = SubscriberRepository::add(product_type, subscriber);
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = product_type_upper.as_str();
+        let subscriber_result = SubscriberRepository::add(product_type_str, subscriber);
         return Ok(subscriber_result);
     }
 
     pub fn unsubscribe(product_type: &str, url: &str) -> Result<Subscriber> {
-        let subscriber_opt = SubscriberRepository::delete(product_type, url);
-        if subscriber_opt.is_none() {
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = product_type_upper.as_str();
+        let result: Option<Subscriber> = SubscriberRepository::delete(product_type_str, url);
+        if result.is_none() {
             return Err(compose_error_response(
                 Status::NotFound,
                 String::from("Subscriber not found.")
             ));
         }
-        return Ok(subscriber_opt.unwrap());
+        return Ok(result.unwrap());
     }
 
     pub fn notify(product_type: &str, notification: Notification) {
